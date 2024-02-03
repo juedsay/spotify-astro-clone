6244b5a8d4b74107ed764fbcd07f08ca2e1973a8
Julian Simón <juedsay@gmail.com>
2024-02-03 12:45:25 -0300

    added slider for SongControl and some bugs resolved

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
 import { Slider } from "./Slider.tsx"
 import { usePlayerStore } from "../store/playerStore"
 
@@ -43,26 +43,80 @@ const CurrentSong = ({ image, title, artists }) => {
     )
 }
 
+const SongControl = ({ audio }) => {
+    const [currentTime, setCurrentTime] = useState(0)
+
+    useEffect(() => {
+        audio.current.addEventListener('timeupdate', handleTimeUpdate)
+        return() => {
+            audio.current.removeEventListener('timeupdate', handleTimeUpdate)
+        }
+    }, [])
+
+    const handleTimeUpdate = () => {
+        setCurrentTime(audio.current.currentTime)
+    }
+
+    const formatTime = time => {
+        if(time == null) return '0:00'
+
+        const seconds = Math.floor(time % 60)
+        const minutes = Math.floor(time / 60)
+
+        return `${minutes}:${seconds.toString().padStart(2, '0')}`
+    }
+
+    const duration = audio?.current?.duration ?? 0
+
+    return (
+        <div className="flex gap-x-3 text-xs pt-2">
+            <span className="opacity-50 w-12 text-right">{formatTime(currentTime)}</span>
+
+            <Slider 
+                    defaultValue={[0]}
+                    value = {[currentTime]}
+                    max={audio?.current?.duration ?? 0}
+                    min={0}
+                    className="w-[400px]"                    
+                    onValueChange={(value)=>{
+                        const [newCurrentTime] = value
+                        audio.current.currentTime = newCurrentTime
+                    }}
+            />
+            
+            <span className="opacity-50 w-12">
+                {duration ? formatTime(duration) : null}
+                </span>
+        </div>
+    )
+}
+
 const VolumeControl = () => {
     const volume = usePlayerStore(state => state.volume)
     const setVolume = usePlayerStore(state => state.setVolume)
     const previousVolumeRef = useRef(volume)
-
+    
+    const isVolumeSilence = volume < 0.1
     const handleClickVolume = () => {
-        setVolume(0)
-    }
-
-    const handleClickSilence = () => {
-
+        if(isVolumeSilence){
+            setVolume(previousVolumeRef.current)
+        }else{
+            previousVolumeRef.current = volume
+            setVolume(0)
+        }
     }
     return (
     <div className="flex justify-center gap-x-2">
+        <button className="opacity-70 hover:opacity-100 transition" onClick={handleClickVolume}>
         {volume < 0.1 ? <VolumeSilence /> : <Volume />}
+
+        </button>
         <Slider 
                     defaultValue={[100]}
                     max={100}
                     min={0}
-                    className="w-[95px]"
+                    value = {[volume * 100]}
+                    className="w-[95px]"                    
                     onValueChange={(value)=>{
                         const [newVolume] = value
                         const volumeValue = newVolume / 100
@@ -102,16 +156,18 @@ export function Player () {
     }
 
     return(
-        <div className="flex flex-row justify-between w-full px-4 z-50">
-            <div>
+        <div className="flex flex-row justify-between w-full px-1 z-50">
+            <div className="w-[200px]" >
                 <CurrentSong {...currentMusic.song}/>
             </div>
 
             <div className="grid place-content-center gap-4 flex-1">
-                <div className="flex justify-center">
+                <div className="flex justify-center flex-col items-center">
                     <button className="bg-white rounded-full p-2" onClick={handleClick}>
                     {isPlaying ? <Pause /> : <Play />}
                     </button>
+                    <SongControl audio={audioRef}  />
+                    <audio ref={audioRef} />
                 </div>
             </div>
 
