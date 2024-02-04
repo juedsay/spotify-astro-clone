ade13ed9863e89105b97e80ea36f10070bfd7c63
Julian Simon <juedsay@gmail.com>
2024-02-04 13:33:42 -0300

    added Playlist Play Button

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Pause, Play } from "./Player"
 import { usePlayerStore } from "../store/playerStore"
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
     const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state);
     const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
     const handleClick = () => {
@@ -25,10 +25,12 @@ export function CardPlayButton({ id }) {
         // const{ songs, playlist, data } = data
 
     }
+    
+    const iconClasName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
 
     return (
-        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
-            { isPlaying ? <Pause /> : <Play /> }
+        <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
+            { isPlaying ? <Pause className={iconClasName} /> : <Play className={iconClasName} /> }
         </button>
     )
 }
